aebbc265e79362dfe76c58aec25469d49cb0ce9f
Mackenly Jones <53151058+mackenly@users.noreply.github.com>
2023-04-29 23:21:28 -0400

    Revert bad fixes

--- src/index.js
+++ src/index.js
@@ -179,9 +179,9 @@ export default {
 			endDate = new Date(today.getFullYear(), today.getMonth(), 14, 23, 59, 59);
 		} else if (today.getDate() > 17 && today.getDate() <= lastDayOfMonth) {
 			// from 15th at midnight to last day of the month at 11:59:59
-			startDate = new Date(today.getFullYear(), today.getMonth(), 13);
+			startDate = new Date(today.getFullYear(), today.getMonth(), 15);
 			// determine the last day of the month
-			endDate = new Date(today.getFullYear(), today.getMonth() + 1, 1, 23, 59, 59);
+			endDate = new Date(today.getFullYear(), today.getMonth() + 1, 0, 23, 59, 59);
 		} else {
 			return new Response('Error: Invalid date');
 		}
